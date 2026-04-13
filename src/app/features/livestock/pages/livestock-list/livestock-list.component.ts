8951d2e63f15673c5a3caa90cfe78bb1ea1effd5
pogipotie <pogipotie@users.noreply.github.com>
2026-04-13 15:46:12 +0800

    Fix missing Inject decorator in Livestock List component

--- src/app/features/livestock/pages/livestock-list/livestock-list.component.ts
+++ src/app/features/livestock/pages/livestock-list/livestock-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, Inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { MatTableModule, MatTableDataSource } from '@angular/material/table';
